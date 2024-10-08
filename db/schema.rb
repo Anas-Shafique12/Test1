8fef88ab311451442e04d0d4a2585069cc3467b4
Anas-Shafique12 <anas.shafique@codingcops.com>
2024-10-08 21:13:04 +0500

    Added resource named as Portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_151345) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_155742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "guides", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

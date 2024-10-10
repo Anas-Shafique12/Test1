d1be7a0c565a12854066a39c0b0a32222fdb963d
Anas-Shafique12 <anas.shafique@codingcops.com>
2024-10-10 20:10:25 +0500

    Custom Routes added in portfolio-feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   resources :posts
   resources :portfolios
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+
+  get "about-me", to: "pages#about"
+  get "contact",  to: "pages#contact"
   resources :guides
+
+  root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

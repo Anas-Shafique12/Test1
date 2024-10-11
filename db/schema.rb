c20dc086431f13a3d9a62f517dde2d21c2dd9b0c
Anas-Shafique12 <anas.shafique@codingcops.com>
2024-10-11 13:02:31 +0500

    friendly id and enum implemented in portfolio-feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_162300) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_10_162123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
 
   create_table "guides", force: :cascade do |t|
     t.string "title"
@@ -35,6 +46,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_162300) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.integer "status", default: 0
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "skills", force: :cascade do |t|

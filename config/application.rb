c7be4fbd0c46abfeda3605fff55f7f3b8d0baee1
Anas-Shafique12 <anas.shafique@codingcops.com>
2024-10-16 21:36:19 +0500

    Guest User Functionality is implemented

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "friendly_id"
+require "ostruct"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
